7545c0ae41b5cbf0f6c3b4ed9b51ea04ca8d1ed4
Denis <denisgreshnaykov@gmail.com>
2022-12-19 23:15:42 +0500

    add single posts

--- page-toys.php
+++ page-toys.php
@@ -42,7 +42,7 @@ Template Name: Игрушки
                     <div class="toys__item-descr">
                         <?php the_field('toys_descr'); ?>                            
                     </div>
-                    <div class="minibutton toys__trigger">Подробнее</div>
+                    <a href="<?php echo get_permalink(); ?>" class="minibutton toys__trigger">Подробнее</a>
                 </div>
             </div>
             
@@ -87,7 +87,7 @@ Template Name: Игрушки
                     <div class="toys__item-descr">
                         <?php the_field('toys_descr'); ?>    
                     </div>
-                    <div class="minibutton toys__trigger">Подробнее</div>
+                    <a href="<?php echo get_permalink(); ?>" class="minibutton toys__trigger">Подробнее</a>
                 </div>
             </div>
 
